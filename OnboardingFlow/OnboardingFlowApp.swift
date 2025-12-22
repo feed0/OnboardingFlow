176c7a39b1fea2cc87f096aa12d2af1bc0d00b02
feed0 <dogmas-gutters.07@icloud.com>
2025-12-22 08:28:50 -0300

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Felipe Eduardo Campelo Ferreira Osorio on 22/12/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
