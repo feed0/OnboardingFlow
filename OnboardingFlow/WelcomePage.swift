764a4ed49f0b17b21477bffd4885771c2109db86
feed0 <dogmas-gutters.07@icloud.com>
2025-12-28 13:40:55 -0300

    [OF-3] Inspect the size of your views using borders
    ADDED in the VStack:
    - Shape
    - Title
    - Description
    
    ADDED: mark comments

--- OnboardingFlow/WelcomePage.swift
+++ OnboardingFlow/WelcomePage.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct WelcomePage: View {
+    
+    // MARK: - Body
+    
     var body: some View {
-        Text("Welcome to My App")
-            .font(.largeTitle)
-            .fontWeight(.semibold)
-            .fontWidth(.expanded)
-            .fontDesign(.serif)
+        VStack {
+            
+            /// Shape
+            RoundedRectangle(cornerRadius: 30)
+                .frame(width: 150, height: 150)
+                .foregroundStyle(.tint)
+            
+            /// Title
+            Text("Welcome to My App")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .fontWidth(.expanded)
+                .fontDesign(.serif)
+            
+                .border(.yellow, width: 1)
+            
+            /// Description
+            Text("This is one of my first SwiftUI apps following the apple developer tutroials")
+                .font(.title2)
+                .multilineTextAlignment(.center)
+            
+                .border(.yellow, width: 1)
+        }
+        .border(.blue, width: 1.5)
+        .padding()
+        .border(.blue, width: 1.5)
     }
 }
+
+// MARK: - Preview
 
 #Preview {
     WelcomePage()
